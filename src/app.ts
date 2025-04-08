324dbdfe3f25bee5111886e6ad2abdedb480cf7c
prem panwala <prem.panwala@bacancy.com>
2025-04-08 19:43:18 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
+
 const app = express();
 dotenv.config();
 require("./routes/demo");
 const PORT = process.env.PORT;
+const path = require("path");
 const outputPath = path.join(__dirname, "dist/routes/output");
 app.use("/output", express.static(outputPath));
 app.get("/", (req: Request, res: Response) => {
